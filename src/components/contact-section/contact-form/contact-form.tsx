6238eeb3010a0410bbfe7c992b182b79329a9501
Yogibear-pixel-99 <joachim.puercher@gmail.com>
2025-09-12 20:28:39 +0200

    testing controlled form function, values, strings, booleans, etc. / created a state and object for the contact form

--- src/components/contact-section/contact-form/contact-form.tsx
+++ src/components/contact-section/contact-form/contact-form.tsx
@@ -1,12 +1,8 @@
 
 import styles from "./contact-form.module.scss";
-
+import { useState } from "react";
 
 export default function ContactForm () {
-const [name, setName] = '';
-const [email, setEmail] = '';
-const [school, setSchool] = '';
-const [question, setQuestion] = '';
 
 
 type contactForm = {
@@ -14,27 +10,58 @@ type contactForm = {
     email: string;
     school: string;
     question: string;
-    privacyCheck: boolean;
+    privacy: boolean;
+}
+
+
+const [contactData, setContactData] = useState<contactForm>({
+    name: '',
+    email: '',
+    school: '',
+    question: '',
+    privacy: false
+})
+
+
+const changeNameValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+
+    setContactData({...contactData, name: event.target.value});
+    console.log(contactData);
+}
+const changeEmailValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+  
+    setContactData({...contactData, email: event.target.value});
+}
+const changeSchoolValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setContactData({...contactData, school: event.target.value});
+
+}
+const changeQuestionValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+     setContactData({...contactData, question: event.target.value});
+
+}
+const changePrivacyValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+     setContactData({...contactData, privacy: event.target.checked});
+
 }
 
 
     return (
-
         <form className={styles['main-content']} action="">
             <label htmlFor="fullname"> Name
-                <input type="text" name="fullname" />
+                <input type="text" name="fullname" value={contactData.name} onChange={changeNameValue}/>
             </label>
             <label htmlFor="email">E-Mail Adresse 
-                <input type="text" name="email" />
+                <input type="text" name="email" value={contactData.email} onChange={changeEmailValue}/>
             </label>
             <label htmlFor="school"> Zu ergänzende Schule
-                <input type="text" name="school" />
+                <input type="text" name="school" value={contactData.school} onChange={changeSchoolValue} />
             </label>
             <label htmlFor="question">Andere Fragen oder Anregungen
-                <input type="text" name="question" />
+                <input type="text" name="question" value={contactData.question} onChange={changeQuestionValue}/>
             </label>
             <label htmlFor="privacy"> Ich akzeptiere die <span><a href="#">Datenschutzerklärung</a>.</span>
-                <input type="checkbox" name="privacy" />
+                <input type="checkbox" name="privacy" checked={contactData.privacy} onChange={changePrivacyValue}/>
             </label>
         </form>
     );
