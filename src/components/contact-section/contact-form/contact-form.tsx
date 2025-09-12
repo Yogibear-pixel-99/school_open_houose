0c7d7846d525cdc8281d83777d40962ee184496a
Yogibear-pixel-99 <joachim.puercher@gmail.com>
2025-09-12 18:09:07 +0200

    started creating react controlled component (form)

---
+++ src/components/contact-section/contact-form/contact-form.tsx
@@ -0,0 +1,22 @@
+
+
+
+
+export default function ContactForm () {
+const [name, setName] = '';
+const [email, setEmail] = '';
+const [school, setSchool] = '';
+const [text, setText] = '';
+
+
+
+    return (
+
+        <form action="">
+
+
+
+
+        </form>
+    );
+}
--- src/components/contact-section/left-content/left-content.tsx
+++ src/components/contact-section/left-content/left-content.tsx
@@ -8,8 +8,8 @@ export default function LeftContactContent () {
 
         <h3 className={`${styles['header']} sub-header-font-small`}>Kontaktiert mich</h3>
         <p className={`${styles['text-box']} main-txt-font`}>Für fehlenden Inhalt, Anregungen, Ideen oder Wünsche bezüglich dieser Seite, kontaktiert mich bitte. Entweder per WhatsApp oder über das vorhanden Kontaktforumlar nebenbei.</p>
-        <p className={`${styles['']}`}>Die Idee dieser Seite ist, alle Infos bezüglich dem Schuljahr und unserer Schule auf einer Seite zu haben. Kein lästiges Zettel suchen oder ständinges nachfragen mehr.</p>
-        <p className={`${styles['']}`}>Alle Infos auf einen Blick, immer und überall, auch am Smartphone.</p>
+        <p className={`${styles['']} main-txt-font`}>Die Idee dieser Seite ist, alle Infos bezüglich dem Schuljahr und unserer Schule auf einer Seite zu haben. Kein lästiges Zettel suchen oder ständinges nachfragen mehr.</p>
+        <p className={`${styles['']} main-txt-font`}>Alle Infos auf einen Blick, immer und überall, auch am Smartphone.</p>
         <div className={`${styles['logos-wrapper']} pad-txt-box`}></div>
             
         </section>
