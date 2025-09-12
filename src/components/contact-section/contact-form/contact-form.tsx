24f30bd54c16266ebcabc41a06634ca70b4fd5a1
Yogibear-pixel-99 <joachim.puercher@gmail.com>
2025-09-12 18:44:41 +0200

    added react form component

--- src/components/contact-section/contact-form/contact-form.tsx
+++ src/components/contact-section/contact-form/contact-form.tsx
@@ -1,22 +1,41 @@
 
-
+import styles from "./contact-form.module.scss";
 
 
 export default function ContactForm () {
 const [name, setName] = '';
 const [email, setEmail] = '';
 const [school, setSchool] = '';
-const [text, setText] = '';
+const [question, setQuestion] = '';
 
+
+type contactForm = {
+    name: string;
+    email: string;
+    school: string;
+    question: string;
+    privacyCheck: boolean;
+}
 
 
     return (
 
-        <form action="">
-
-
-
-
+        <form className={styles['main-content']} action="">
+            <label htmlFor="fullname"> Name
+                <input type="text" name="fullname" />
+            </label>
+            <label htmlFor="email">E-Mail Adresse 
+                <input type="text" name="email" />
+            </label>
+            <label htmlFor="school"> Zu ergänzende Schule
+                <input type="text" name="school" />
+            </label>
+            <label htmlFor="question">Andere Fragen oder Anregungen
+                <input type="text" name="question" />
+            </label>
+            <label htmlFor="privacy"> Ich akzeptiere die <span><a href="#">Datenschutzerklärung</a>.</span>
+                <input type="checkbox" name="privacy" />
+            </label>
         </form>
     );
 }
--- src/components/contact-section/right-content/right-content.tsx
+++ src/components/contact-section/right-content/right-content.tsx
@@ -1,12 +1,13 @@
 
 
+import ContactForm from "../contact-form/contact-form";
 import styles from "./right-content.module.scss";
 
 export default function RightContactContent () {
     return (
         <section className={`${styles['main-content']} pad-txt-box std-glas-effect-shadow border-txt-box`}>
 
-        
+        <ContactForm />
             
         </section>
     );
