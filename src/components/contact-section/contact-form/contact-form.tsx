4e08e3e3116dcd0deacb20fafbad3a819d3ca344
Yogibear-pixel-99 <joachim.puercher@gmail.com>
2025-09-14 18:12:50 +0200

    Testing forms with react, useState, errorhandling etc.

--- src/components/contact-section/contact-form/contact-form.tsx
+++ src/components/contact-section/contact-form/contact-form.tsx
@@ -2,21 +2,45 @@ import styles from "./contact-form.module.scss";
 import { useState } from "react";
 
 export default function ContactForm() {
-  type contactForm = {
+  type ContactForm = {
     name: string;
     email: string;
     school: string;
-    question: string;
+    message: string;
     privacy: boolean;
   };
 
-  const [contactData, setContactData] = useState<contactForm>({
+  type ContactFormPlaceholder = {
+    nameEmpty: string;
+    emailEmpty: string;
+    messageEmpty: string;
+    schoolEmpty: string;
+    privacyNotChecked: string;
+  };
+
+  const [errors, setErrors] = useState({
+    name: false,
+    email: false,
+    message: false,
+    privacy: false
+  })
+
+  const [contactData, setContactData] = useState<ContactForm>({
     name: "",
     email: "",
     school: "",
-    question: "",
+    message: "",
     privacy: false,
   });
+
+  const [contactDataPlaceholder, setContactDataPlaceholder] =
+    useState<ContactFormPlaceholder>({
+      nameEmpty: "Bitte Namen eingeben",
+      emailEmpty: "Bitte EMail Adresse eingeben",
+      messageEmpty: "Hier eine kurze Nachricht eintragen",
+      schoolEmpty: "Bitte den Namen der Schule eintragen",
+      privacyNotChecked: "Bitte die Datenschutzerklärung akzeptieren",
+    });
 
   const changeNameValue = (event: React.ChangeEvent<HTMLInputElement>) => {
     setContactData({ ...contactData, name: event.target.value });
@@ -28,63 +52,114 @@ export default function ContactForm() {
     setContactData({ ...contactData, school: event.target.value });
   };
   const changeQuestionValue = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setContactData({ ...contactData, question: event.target.value });
+    setContactData({ ...contactData, message: event.target.value });
   };
   const changePrivacyValue = (event: React.ChangeEvent<HTMLInputElement>) => {
     setContactData({ ...contactData, privacy: event.target.checked });
   };
 
+  const sentInfos = (event: any) => {
+    event.preventDefault();
+    let testSent = false;
+    let formData = contactData;
+    checkIfEmpty();
+
+    console.log(testSent);
+    console.log(formData);
+  };
+
+  const checkIfEmpty = () => {
+    const updatedPlaceholder = { ...contactDataPlaceholder };
+    const updateErrorClasses = { ...errors};
+
+    if (contactData.name === "") {
+      updatedPlaceholder.nameEmpty = "Leider fehlt dein Name";
+      updateErrorClasses.name = true;
+    }
+
+    if (contactData.email === "") {
+      updatedPlaceholder.emailEmpty = "Ups, leider keine E-Mail Adresse";
+      updateErrorClasses.email = true;
+    }
+
+    if (contactData.message === "") {
+      updatedPlaceholder.messageEmpty = "Bitte schreib eine kurze Info";
+      updateErrorClasses.message = true;
+    }
+
+    if (contactData.privacy === false) {
+      updatedPlaceholder.privacyNotChecked = "Bitte die Datenschutzerklärung akzeptieren";
+      updateErrorClasses.privacy = true;
+    }
+
+    setContactDataPlaceholder(updatedPlaceholder);
+    setErrors(updateErrorClasses);
+  };
+
   return (
-    <form className={styles["main-content"]} action="">
-      <label className='input-label-font' htmlFor="fullname">
+    <form className={styles["main-content"]} onSubmit={sentInfos}>
+      <label className="input-label-font" htmlFor="fullname">
         {" "}
         Name
-        <input className="input-placeholder-font"
+        <input
+          className={`${errors.name ? styles['error'] : ''} input-placeholder-font`}
           type="text"
           id="fullname"
           name="fullname"
+          maxLength={20}
+          placeholder={contactDataPlaceholder.nameEmpty}
           value={contactData.name}
           onChange={changeNameValue}
         />
       </label>
-      <label className='input-label-font' htmlFor="email">
+      <label className="input-label-font" htmlFor="email">
         E-Mail Adresse
-        <input className="input-placeholder-font"
+        <input
+          className={`${errors.name ? styles['error'] : ''} input-placeholder-font`}
           type="text"
           id="email"
           name="email"
+          maxLength={30}
+          placeholder={contactDataPlaceholder.emailEmpty}
           value={contactData.email}
           onChange={changeEmailValue}
         />
       </label>
-      <label className='input-label-font' htmlFor="school">
+      <label className="input-label-font" htmlFor="school">
         {" "}
         Zu ergänzende Schule
-        <input className="input-placeholder-font"
+        <input
+          className={`${errors.name ? styles['error'] : ''} input-placeholder-font`}
           type="text"
           id="school"
           name="school"
+          maxLength={30}
+          placeholder={contactDataPlaceholder.schoolEmpty}
           value={contactData.school}
           onChange={changeSchoolValue}
         />
       </label>
-      <label className='input-label-font' htmlFor="question">
+      <label className="input-label-font" htmlFor="question">
         Andere Fragen oder Anregungen
-        <input className="input-placeholder-font"
+        <input
+          className={`${errors.name ? styles['error'] : ''} input-placeholder-font`}
           type="text"
           name="question"
           id="question"
-          value={contactData.question}
+          maxLength={200}
+          placeholder={contactDataPlaceholder.messageEmpty}
+          value={contactData.message}
           onChange={changeQuestionValue}
         />
       </label>
-      <label className='input-label-font' htmlFor="privacy">
+      <label className="input-label-font" htmlFor="privacy">
         {" "}
         <span>
           <a href="#">Datenschutzerklärung</a>.
         </span>
         Ich akzeptiere die{" "}
-        <input className="input-placeholder-font"
+        <input
+          className="input-placeholder-font"
           type="checkbox"
           name="privacy"
           id="privacy"
@@ -93,7 +168,9 @@ export default function ContactForm() {
         />
       </label>
 
-      <button className="button-std-font">Abschicken</button>
+      <button type="submit" className="button-std-font">
+        Abschicken
+      </button>
     </form>
   );
 }
