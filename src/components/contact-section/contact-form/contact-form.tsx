553fec984951af7dabfb91a2aeba05865f58b7e9
Yogibear-pixel-99 <joachim.puercher@gmail.com>
2025-09-17 14:27:49 +0200

    testing error handling in form

--- src/components/contact-section/contact-form/contact-form.tsx
+++ src/components/contact-section/contact-form/contact-form.tsx
@@ -22,8 +22,15 @@ export default function ContactForm() {
     name: false,
     email: false,
     message: false,
-    privacy: false
-  })
+    privacy: false,
+  });
+
+  let checkErrors = {
+    name: true,
+    email: true,
+    message: true,
+    privacy: true,
+  };
 
   const [contactData, setContactData] = useState<ContactForm>({
     name: "",
@@ -58,51 +65,88 @@ export default function ContactForm() {
     setContactData({ ...contactData, privacy: event.target.checked });
   };
 
-  const sentInfos = (event: any) => {
+  const handleSubmit = (event: any) => {
     event.preventDefault();
-    let testSent = false;
-    let formData = contactData;
-    checkIfEmpty();
 
-    console.log(testSent);
-    console.log(formData);
+    checkIfEmpty();
+    debugger;
+    if (Object.values(checkErrors).every((value) => value === false)) {
+      sendData();
+    }
   };
 
   const checkIfEmpty = () => {
     const updatedPlaceholder = { ...contactDataPlaceholder };
-    const updateErrorClasses = { ...errors};
+    const updateErrorClasses = { ...errors };
 
     if (contactData.name === "") {
       updatedPlaceholder.nameEmpty = "Leider fehlt dein Name";
       updateErrorClasses.name = true;
+    } else {
+      updateErrorClasses.name = false;
     }
 
     if (contactData.email === "") {
       updatedPlaceholder.emailEmpty = "Ups, leider keine E-Mail Adresse";
       updateErrorClasses.email = true;
+    } else {
+      updateErrorClasses.email = false;
     }
 
     if (contactData.message === "") {
       updatedPlaceholder.messageEmpty = "Bitte schreib eine kurze Info";
       updateErrorClasses.message = true;
+    } else {
+      updateErrorClasses.message = false;
     }
 
     if (contactData.privacy === false) {
-      updatedPlaceholder.privacyNotChecked = "Bitte die Datenschutzerklärung akzeptieren";
+      updatedPlaceholder.privacyNotChecked =
+        "Bitte die Datenschutzerklärung akzeptieren";
       updateErrorClasses.privacy = true;
+    } else {
+      updateErrorClasses.privacy = false;
     }
 
+    checkErrors = updateErrorClasses;
     setContactDataPlaceholder(updatedPlaceholder);
     setErrors(updateErrorClasses);
   };
 
+  const endPoint: string = "https://www.puercherjoachim.com/sendMailSchool.php";
+
+  const sendData = async () => {
+    debugger;
+    try {
+      const response = await fetch(endPoint, {
+        method: "POST",
+        headers: {
+          "Content-Type": "text/plain",
+          responseType: "text",
+        },
+        body: JSON.stringify(contactData),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Fehler: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error("Fehler beim Senden:", (error as Error).message);
+    }
+  };
+
   return (
-    <form className={styles["main-content"]} onSubmit={sentInfos}>
+    <form className={styles["main-content"]} onSubmit={handleSubmit}>
       <label className="input-label-font" htmlFor="fullname">
         {" "}
         Name
         <input
-          className={`${errors.name ? styles['error'] : ''} input-placeholder-font`}
+          className={`${
+            errors.name ? styles["error"] : ""
+          } input-placeholder-font`}
           type="text"
           id="fullname"
           name="fullname"
@@ -115,7 +159,9 @@ export default function ContactForm() {
       <label className="input-label-font" htmlFor="email">
         E-Mail Adresse
         <input
-          className={`${errors.name ? styles['error'] : ''} input-placeholder-font`}
+          className={`${
+            errors.name ? styles["error"] : ""
+          } input-placeholder-font`}
           type="text"
           id="email"
           name="email"
@@ -129,7 +175,9 @@ export default function ContactForm() {
         {" "}
         Zu ergänzende Schule
         <input
-          className={`${errors.name ? styles['error'] : ''} input-placeholder-font`}
+          className={`${
+            errors.name ? styles["error"] : ""
+          } input-placeholder-font`}
           type="text"
           id="school"
           name="school"
@@ -142,7 +190,9 @@ export default function ContactForm() {
       <label className="input-label-font" htmlFor="question">
         Andere Fragen oder Anregungen
         <input
-          className={`${errors.name ? styles['error'] : ''} input-placeholder-font`}
+          className={`${
+            errors.name ? styles["error"] : ""
+          } input-placeholder-font`}
           type="text"
           name="question"
           id="question"
