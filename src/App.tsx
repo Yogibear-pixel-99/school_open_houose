76761149a700897d225f11ad8c7ee0b56a81efc5
Yogibear-pixel-99 <joachim.puercher@gmail.com>
2025-09-22 14:40:04 +0200

    added my own visitor tracking method by a real time database

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,51 @@ import Footer from "./components/footer/footer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ScrollToElement from "./shared/logic/scrollToElement";
 import PrivacyPolicy from "./components/privacy-policy/privacy-policy";
+import { useEffect } from "react";
 
 
-function App() {
+export default function App() {
+
+useEffect(() => {
+const apiRequest = async () => {
+  await getVisitors();
+ 
+}
+apiRequest();
+  }, []);
+
+  async function getVisitors() {
+    try {
+      const response = await fetch("https://school-infos-default-rtdb.europe-west1.firebasedatabase.app/.json");
+
+      if (!response.ok) {
+        throw new Error (`Response status: ${response.status}`)
+      }
+      const data = await response.json();
+      let visitorsNumber = data.visitors;
+      visitorsNumber++;
+       await pushVisitors(visitorsNumber);
+
+    } catch (error) {
+       console.error((error as Error).message);
+    }
+  }
+
+  async function pushVisitors(payload:number) {
+try { const response = await fetch("https://school-infos-default-rtdb.europe-west1.firebasedatabase.app/.json", {
+   method: "PUT",
+        headers: {
+          "Content-Type": "application/plain"
+        },
+        body: JSON.stringify({visitors: payload}),
+});
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+} catch (error) {
+  console.error("Fehler beim Senden:", (error as Error).message);
+}
+  }
 
   return (
     <BrowserRouter basename="/schoolinfos">
@@ -22,5 +64,5 @@ function App() {
   );
 }
 
-export default App;
+
 
